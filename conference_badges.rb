7809800e6818226f5270cd5eaa39f973702ca528
James Mobus <mobusj@live.com>
2019-08-20 23:12:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,3 +14,10 @@ def assign_rooms(attendees)
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end
 end
+
+def printer(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    puts badge_maker(attendee)
+    puts "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+end
