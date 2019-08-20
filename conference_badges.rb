246be12a8bc069c781260dbe129323870ccd0597
James Mobus <mobusj@live.com>
2019-08-20 23:11:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map |attendee, index|
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}"
+  end
+end
