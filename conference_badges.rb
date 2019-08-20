8d8e635d91c84fde0123678b17fa4e4386b7a5ea
James Mobus <mobusj@live.com>
2019-08-20 23:09:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def badge_batch_creator(attendees)
+def batch_badge_creator(attendees)
   attendees.map do |attendee|
     badge_maker(attendee)
   end
